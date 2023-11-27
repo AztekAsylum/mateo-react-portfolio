1cab31352e40d3ae8a6f14b6b4a3f68ef4c0ed31
AztekAsylum <mateo.rodriguez85@gmail.com>
2023-11-27 13:40:23 -0700

    updated project page descriptions

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,7 +3,7 @@ export const projectData = [
     _id: 1,
     title: "HACK THE PLANET",
     description:
-      "this is an e-commerce website for tech enthusiasts with latest and greatest offerings from the tech industry with full checkout functionality through stripe ",
+      "This is an e-commerce website for tech enthusiasts with latest and greatest offerings from the tech industry with full checkout functionality through stripe. Technologies used include JavaScript, ReactJS, HTML, CSS, GraphQL, Node.js, Exrpess.js, Stripe, & MongoDB.",
     imageUrl: "/Assets/Images/HackThePlanet.png",
     deployedUrl: "https://devshacktheplanet-44c29a23ae44.herokuapp.com/",
     repoUrl: "https://github.com/AlexPurfield/hack_the_planet",
@@ -12,7 +12,7 @@ export const projectData = [
     _id: 2,
     title: "DEV ROW RECORDS",
     description:
-      " online music platform, where you can discover and support talented artists",
+      "This is a mock record label collaborative project. Users can create an account, browse artists, albums, merch, and add to favorites to revisit at a future date or time. Technologies used include JavaScript, Handlebars, CSS, Node.js, MySQL, HTML, & Express.js",
     imageUrl: "/Assets/Images/DevRowRecords.png",
     deployedUrl: "https://dev-row-records-63d750921ea0.herokuapp.com/",
     repoUrl: "https://github.com/PRich57/dev-row-records",
@@ -21,7 +21,7 @@ export const projectData = [
     _id: 3,
     title: "GAME SEEKERS",
     description:
-      "video game recommendation web application tailored to your preferences",
+      "Game Seekers is a tailored application that helps gamers discover their next favorite video games by evaluating their current preferences and cross-referencing this information with a comprehensive video game API database. Technologies used include JavaScript, HTML, CSS, JQuery,Third-Party APIs, & Bootstrap.",
     imageUrl: "/Assets/Images/GameSeekers.png",
     deployedUrl: "https://ljkahn.github.io/videoGameProj/",
     repoUrl: "https://github.com/ljkahn/videoGameProj",
@@ -30,7 +30,7 @@ export const projectData = [
     _id: 4,
     title: "PORTFOLIO",
     description:
-      "website portfolio showcaseing my work that is built with using html and css",
+      "My initial endeavor involves creating a website portfolio that highlights my photography and design expertise stemming from a background in graphic design. The site features a sleek, modern,uncomplicated layout. Technologies used include HTML & CSS.",
     imageUrl: "/Assets/Images/PortfolioHTML.png",
     deployedUrl: "https://aztekasylum.github.io/portfolio/",
     repoUrl: "https://github.com/AztekAsylum/portfolio",
@@ -39,7 +39,7 @@ export const projectData = [
     _id: 5,
     title: "WORK DAY SCHEDULER",
     description:
-      "calendar application that allows a user to save work events in a 9am-5pm day",
+      "Calendar app that displays time blocks for the standard working hours, color-coding to distinguish between past, present, and future events. Users can click on time blocks to input information, save their entries to local storage, and upon refreshing the page, the saved events persist. Technologies used include HTML, CSS, JavaScript, Day.js, & jQuery.",
     imageUrl: "/Assets/Images/WorkDayScheduler.png",
     deployedUrl: "https://aztekasylum.github.io/work-day-scheduler/",
     repoUrl: "https://github.com/AztekAsylum/work-day-scheduler",
@@ -48,7 +48,7 @@ export const projectData = [
     _id: 6,
     title: "NOTE TAKER",
     description:
-      "this is an app to create and save notes as well as delete for your daily needs",
+      "This application allows users to create, save, and delete notes. It can be used for your daily needs such as grocery shopping, tasks around the house, & more. Technologies used include HTML, CSS, JavaScript, & Express.js",
     imageUrl: "/Assets/Images/NoteTaker.png",
     deployedUrl: "https://note-take-mateo-6a02b7130490.herokuapp.com/",
     repoUrl: "https://github.com/AztekAsylum/note-taker",
